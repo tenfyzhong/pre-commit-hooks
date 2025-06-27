f5b0c5db9b6c0a569a78316995a807f9440d0319
tenfyzhong <tenfy@tenfy.cn>
2025-06-27 21:29:14 +0800

    feat(make-target): Run make commands from Git root
    - Ensures `make` commands are executed from the Git repository's root directory.
    - This prevents `make` failures or unexpected behavior when the `make-target.sh` script is run from a subdirectory.
    - The script changes to the Git root before running `make` and then restores the original working directory.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- make-target.sh
+++ make-target.sh
@@ -6,6 +6,10 @@ if [ "$#" -eq 0 ]; then
     exit 0
 fi
 
+# cd the git root directory
+cwd=$(pwd)
+cd "$(git rev-parse --show-toplevel)"
+
 for target in "$@"; do
     if ! make -n "$target" >/dev/null 2>&1; then
       echo "make target '$target' not found, skipping."
@@ -14,3 +18,5 @@ for target in "$@"; do
 
     make "$target"
 done
+
+cd "$cwd"
